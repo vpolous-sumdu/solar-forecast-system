26cb8e518c30f23f851cbb9c43b9a50ccccba074
Vladyslav Polous <v.polous@sumdu.edu.ua>
2026-08-08 16:57:07 +0300

    fix backend import errors and update render.yaml startCommand with PYTHONPATH configuration

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,12 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.db.session import engine, Base
 from app.api.stations import router as stations_router
+
 
 # Автоматично створюємо таблиці у PostgreSQL при старті (якщо їх ще немає)
 Base.metadata.create_all(bind=engine)
